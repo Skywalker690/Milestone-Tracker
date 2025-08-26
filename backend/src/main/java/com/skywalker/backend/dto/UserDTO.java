9ae9ff0f07509aa82d410aa55def5f238b043df3
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-26 17:39:45 +0530

    feat: add UserDTO with non-null JSON serialization

---
+++ backend/src/main/java/com/skywalker/backend/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.skywalker.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
