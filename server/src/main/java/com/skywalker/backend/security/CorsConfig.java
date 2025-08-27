76f32266734564c2e692766f400eb2a443e4a45b
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-27 20:39:28 +0530

    Refactor

--- server/src/main/java/com/skywalker/backend/security/CorsConfig.java
+++ server/src/main/java/com/skywalker/backend/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Frontend URL
+                        .allowedOrigins("https://milestone-tracker-e1nkp9kuy-skywalker690s-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
