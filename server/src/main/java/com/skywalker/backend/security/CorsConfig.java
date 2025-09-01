3ce0c9b7e0aa4d5ca0b72efd02abb38c69a2f9b3
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-01 19:12:56 +0530

    Fix Cors Error

--- server/src/main/java/com/skywalker/backend/security/CorsConfig.java
+++ server/src/main/java/com/skywalker/backend/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://milestone-tracker-phi.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
